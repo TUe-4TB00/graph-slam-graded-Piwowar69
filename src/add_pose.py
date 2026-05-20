1fb7886e6deee9bec4591a4c947a18147faf00d1
Jan Piwko <j.p.piwko@student.tue.nl>
2026-05-20 19:33:22 +0200

    Is there something wrong?

--- src/add_pose.py
+++ src/add_pose.py
@@ -10,7 +10,8 @@ MEASUREMENT_NOISE = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.05, 0.1]))  # (
 
 def add_pose(graph, initial_estimate):
     # TODO: Add the odometry factor between X(4) and X(5) to the graph (BetweenFactorPose2)
-
+        angle = np.deg2rad(90)
+        graph.add(gtsam.BetweenFactorPose2(X(3), X(4), gtsam.Pose2(2.0, 0.0,angle),ODOMETRY_NOISE))
     # TODO: Based on the odometry, find the initial estimate for the pose of X(5) and add it to the graph
-    
-    return graph, initial_estimate
+        initial_estimate.insert(X(4), gtsam.Pose2(2, 0.0,angle))
+        return graph,initial_estimate 
