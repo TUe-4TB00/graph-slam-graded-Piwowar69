7c716110f9a238a4ad1aab2911fdb94fc3f75868
Jan Piwko <j.p.piwko@student.tue.nl>
2026-05-27 10:43:27 +0200

    Commit test

--- src/pose_5.py
+++ src/pose_5.py
@@ -73,6 +73,6 @@ def minimize_errors(graph, initial_estimate, pose_options):
     # TODO: create a list of errors (each index corresponds to a pose) and add the error of each pose to the list
     list_of_errors = []
     # TODO: compute the sum of the errors and return it along with the best pose and landmark
-    sum_of_errors = 0
+    sum_of_errors = sum(list_of_errors)
 
     return best_pose, best_landmark, sum_of_errors 
